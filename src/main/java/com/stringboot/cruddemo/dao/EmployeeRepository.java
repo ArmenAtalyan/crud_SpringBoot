99641191d3a8d3a528b2891f2bd23127b9401662
ArmenAtalyan <atalyan3@gmail.com>
2020-05-25 00:36:47 +0400

    first commit

--- src/main/java/com/stringboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/stringboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.stringboot.cruddemo.dao;
 
 import com.stringboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
